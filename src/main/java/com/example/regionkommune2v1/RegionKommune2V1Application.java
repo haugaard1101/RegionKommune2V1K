f3ea650cedad2d2c4afa051e50e94751713e4c31
ERLMKea <erlm@kea.dk>
2023-03-03 09:20:05 +0100

    Initial commit

---
+++ src/main/java/com/example/regionkommune2v1/RegionKommune2V1Application.java
@@ -0,0 +1,13 @@
+package com.example.regionkommune2v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionKommune2V1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionKommune2V1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/regionkommune2v1/RegionKommune2V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regionkommune2v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionKommune2V1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
